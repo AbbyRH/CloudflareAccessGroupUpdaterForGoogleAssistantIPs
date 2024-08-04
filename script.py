722eb9b75e147def1db68c5feb60a6e626f8ab76
Abigail Howe <abby@abigailhowe.org>
2024-08-04 23:09:38 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,26 @@
 import CloudFlare
 import requests
+from netaddr import IPNetwork, IPSet
+
+def get_google_ips():
+    """Get list of CIDR-notation Google subnets not assigned to GCP customers (and therefore used for google's APIs and services)"""
+    logging.debug('Getting Google API IPs')
+    all_ips = get_ip_set(config['all_google_ips_url'])
+    customer_ips = get_ip_set(config['google_cloud_ips_url'])
+    api_ips = all_ips - customer_ips
+    return [{"ip": str(ip)} for ip in api_ips.iter_cidrs()]
+
+def get_ip_set(url):
+    try:
+        json_data = requests.get(url).json()
+    except:
+        logging.exception(f"Error getting JSON data from '{url}'")
+        raise
+    try:
+        return IPSet([next(iter(i.values())) for i in json_data['prefixes']])
+    except:
+        logging.exception(f"Error adding CIDR blocks to IPSet")
+        raise
 
 def get_google_addresses():
     r = requests.get("https://www.gstatic.com/ipranges/goog.json")
@@ -24,6 +45,6 @@ if __name__ == "__main__":
     parser.add_argument("--group",   required=True, help="ID of CF Access group that needs to be updated")
     args = parser.parse_args()
 
-    ips = get_google_addresses()
+    ips = get_google_ips()
     update_access_group(args.token, args.account, args.group, ips)
   
